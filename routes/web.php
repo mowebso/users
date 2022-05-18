7da7dc33168e7a0994e2c5ede145c037bad3dc0d
eugenstranz <eugen.stranz@icloud.com>
2022-05-18 18:08:57 +0800

    π

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use MoWebSo\Tenants\Controllers\ChangeTenantController;
-use MoWebSo\Users\Controller\UserController;
-use MoWebSo\Users\Models\User;
 
 Route::macro('Users', function (string $baseUrl = 'users')
 {
     Route::prefix($baseUrl)
          ->group(function ()
          {
-             // Change Current Tenant For User
-             // Route::get('change/{toTenantID}', ChangeTenantController::class)
-             //      ->name('tenant.change');
-
 
          });
 });
